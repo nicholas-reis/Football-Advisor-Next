3854c99951752e7cb4ec31d6004b564c49fe2911
Nicholas Reis <nicholas_reis@outlook.com>
2022-07-20 21:07:12 -0700

    test

--- next.config.js
+++ next.config.js
@@ -63,4 +63,13 @@ module.exports = withTM({ // Your Next.js configuration
 
     return config;
   },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    //mySecret: 'secret',
+    //secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/data',
+  },
 });
